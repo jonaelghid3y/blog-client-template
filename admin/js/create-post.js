e00d3f5b69d6d4eb5cf7685f66450e44c6002f48
jonaelghid3y <jonaelghidey@hotmail.com>
2022-12-13 13:56:43 +0100

    klar med funktionella

--- admin/js/create-post.js
+++ admin/js/create-post.js
@@ -1,128 +1,121 @@
 class CustomSelect {
-    constructor(originalSelect) {
-      this.originalSelect = originalSelect;
-      this.customSelect = document.createElement("div");
-      this.customSelect.classList.add("select");
-  
-      this.originalSelect.querySelectorAll("option").forEach((optionElement) => {
-        const itemElement = document.createElement("div");
-  
-        itemElement.classList.add("select__item");
-        itemElement.textContent = optionElement.textContent;
-        this.customSelect.appendChild(itemElement);
-  
-        if (optionElement.selected) {
+  constructor(originalSelect) {
+    this.originalSelect = originalSelect;
+    this.customSelect = document.createElement("div");
+    this.customSelect.classList.add("select");
+
+    this.originalSelect.querySelectorAll("option").forEach((optionElement) => {
+      const itemElement = document.createElement("div");
+
+      itemElement.classList.add("select__item");
+      itemElement.textContent = optionElement.textContent;
+      this.customSelect.appendChild(itemElement);
+
+      if (optionElement.selected) {
+        this._select(itemElement);
+      }
+
+      itemElement.addEventListener("click", () => {
+        if (
+          this.originalSelect.multiple &&
+          itemElement.classList.contains("select__item--selected")
+        ) {
+          this._deselect(itemElement);
+        } else {
           this._select(itemElement);
         }
-  
-        itemElement.addEventListener("click", () => {
-          if (
-            this.originalSelect.multiple &&
-            itemElement.classList.contains("select__item--selected")
-          ) {
-            this._deselect(itemElement);
-          } else {
-            this._select(itemElement);
-          }
-        });
       });
-  
-      this.originalSelect.insertAdjacentElement("afterend", this.customSelect);
-      this.originalSelect.style.display = "none";
-    }
-  
-    _select(itemElement) {
-      const index = Array.from(this.customSelect.children).indexOf(itemElement);
-  
-      if (!this.originalSelect.multiple) {
-        this.customSelect.querySelectorAll(".select__item").forEach((el) => {
-          el.classList.remove("select__item--selected");
-        });
-      }
-  
-      this.originalSelect.querySelectorAll("option")[index].selected = true;
-      itemElement.classList.add("select__item--selected");
-    }
-  
-    _deselect(itemElement) {
-      const index = Array.from(this.customSelect.children).indexOf(itemElement);
-  
-      this.originalSelect.querySelectorAll("option")[index].selected = false;
-      itemElement.classList.remove("select__item--selected");
-    }
+    });
+
+    this.originalSelect.insertAdjacentElement("afterend", this.customSelect);
+    this.originalSelect.style.display = "none";
   }
-  
-  document.querySelectorAll(".custom-select").forEach((selectElement) => {
-    new CustomSelect(selectElement);
-  });
-  
+
+  _select(itemElement) {
+    const index = Array.from(this.customSelect.children).indexOf(itemElement);
+
+    if (!this.originalSelect.multiple) {
+      this.customSelect.querySelectorAll(".select__item").forEach((el) => {
+        el.classList.remove("select__item--selected");
+      });
+    }
+
+    this.originalSelect.querySelectorAll("option")[index].selected = true;
+    itemElement.classList.add("select__item--selected");
+  }
+
+  _deselect(itemElement) {
+    const index = Array.from(this.customSelect.children).indexOf(itemElement);
+
+    this.originalSelect.querySelectorAll("option")[index].selected = false;
+    itemElement.classList.remove("select__item--selected");
+  }
+}
+
+document.querySelectorAll(".custom-select").forEach((selectElement) => {
+  new CustomSelect(selectElement);
+});
+/*--------------------------------------------- ovan är animation för tags-------------------------------------------------------------*/
 
 document.getElementById('create-pun-form').addEventListener('submit', async function (e) {
-    e.preventDefault();
-    const form = e.target;
-    
-    let titles = document.getElementById('titel')
-    let authors = document.getElementById('author')
-    let texts = document.getElementById('content-textarea')
-    let selection1 = document.getElementById('select1')
-    let selection2 = document.getElementById('select2')
-    let selection3 = document.getElementById('select3')
-    
-    
-    
+  e.preventDefault()
+  const form = e.target
 
-    let tags = [selection1.value , selection2.value, selection3.value]
-    
-  console.log(tags)
-    const blogpost = {
 
-        title: titles.value,
-        author: authors.value,
-        content: texts.value,
-        tags: tags
+  let serializeForm = function (form) {
 
+    var obj = {};
+    var formData = new FormData(form);
+
+
+    for (var key of formData.keys()) {
+      let inputData = formData.getAll(key);
+
+      if (inputData.length > 1) {
+        obj[key] = inputData;
+      } else {
+        obj[key] = inputData[0];
+      }
     }
- 
+
+    return obj;
+  };
+  let formDataObjekt = serializeForm(form);
 
 
 
-    // let formDataObject = serializeForm(form);
+  let titles = document.getElementById('titel')
+  let authors = document.getElementById('author')
+  let texts = document.getElementById('content-textarea')
+  let selection1 = document.getElementById('select1')
 
-    //console.log(formDataObject)
-    try {
-        await fetch('https://blog-api-assignment.up.railway.app/posts', {
-            method: 'POST', // *GET, POST, PUT, DELETE, etc.
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(blogpost), // body data type must match "Content-Type" header
 
-        })
+  console.log(formDataObjekt.tags)
 
-        //location.replace('index.html')
-    } catch (error) {
-        console.log(error)
-    }
+  const blogpost = {
+
+    title: titles.value,
+    author: authors.value,
+    content: texts.value,
+    tags: formDataObjekt.tags
+
+  }
+
+  try {
+    await fetch('https://blog-api-assignment.up.railway.app/posts', {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(blogpost), // body data type must match "Content-Type" header
+
+    })
+
+    location.replace('index.html')
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 
 
-
-/*let serializeForm = function (form) {
-    var obj = {};
-    var formData = new FormData(form);
-    console.log(formData.keys());
-
-    for (var key of formData.keys()) {
-        let inputData = formData.getAll(key);
-
-        if (inputData.length > 1) {
-            obj[key] = inputData;
-        } else {
-            obj[key] = inputData[0];    
-        }
-    }
-    
-    // console.log(obj);
-    return obj;
-};*/
--- admin/js/manage-posts.js
+++ admin/js/manage-posts.js
@@ -4,7 +4,7 @@ async function fetchAllbloggs() {
         let blogs = await response.json();
 
         let blogHTML = '';
-        for(let blog of blogs) {
+        for (let blog of blogs) {
             let blogDate = new Date(blog.date)
 
             blogHTML += `
@@ -15,60 +15,35 @@ async function fetchAllbloggs() {
             <td>${blogDate.getFullYear()}-${blogDate.getMonth()}-${blogDate.getDate()}</td>
             <td> ${blog.tags}</td>
             <td> <a class="uppdatebtn" href="update-post.html?id=${blog._id}">ändra</a><button class="deletebtn" href="#" data-id="${blog._id}">Ta bort</button></td>
-           
-         
-
           </tr>
-            
-            
-            
-            `
-            /*`
-            <li class="list-group-item">
-                    <p>${blog.content} <br> <span class="date">- ${blogDate.getFullYear()}-${blogDate.getMonth() + 1}-${blogDate.getDate()} ${blogDate.getHours()}-${blogDate.getMinutes()}-${blogDate.getSeconds()}</span> </p>
-                    
-                    <div>
-                        <a href="update-pun.html?id=${pun._id}" >Update</a> |
-                        <a href="#" class="delete-links" data-id="${pun._id}">Delete</a> 
-                    </div>
-                </li>
-            ` */
+                 `
         }
 
         document.getElementById('table').innerHTML += blogHTML;
 
-    } catch(error) {
+    } catch (error) {
         console.log(error);
     }
     const deleteLinks = document.getElementsByClassName('deletebtn');
-    for(let link of deleteLinks) {
-        
-        
-    
-        link.addEventListener('click', async function(e) {
+    for (let link of deleteLinks) {
+
+        link.addEventListener('click', async function (e) {
             e.preventDefault();
             console.log(e.target);
             console.log(e.target.dataset.id);
-    
+
             try {
-                const response = await fetch('https://blog-api-assignment.up.railway.app/posts/'+ e.target.dataset.id, {
-                    method:  'DELETE'
+                const response = await fetch('https://blog-api-assignment.up.railway.app/posts/' + e.target.dataset.id, {
+                    method: 'DELETE'
                 });
-    
+
                 e.target.parentNode.parentNode.remove();
 
-                
-            } catch(error) {
+
+            } catch (error) {
                 console.log(error);
             }
         })
     }
-   
-    
-
 }
-
-
-
-
 fetchAllbloggs();
